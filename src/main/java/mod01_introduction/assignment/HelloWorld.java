f283cd0403835856e4273912f759c569b5d41010
Ivan Rodrigues <ivanho92@hotmail.be>
2024-11-24 15:47:05 +0100

    chore: prefixed modules with leading zero

---
+++ src/main/java/mod01_introduction/assignment/HelloWorld.java
@@ -0,0 +1,9 @@
+package mod01_introduction.assignment;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        StdOut.println("Hello, World");
+    }
+}
--- src/main/java/mod1_introduction/assignment/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package mod1_introduction.assignment;
-
-import edu.princeton.cs.algs4.StdOut;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        StdOut.println("Hello, World");
-    }
-}
